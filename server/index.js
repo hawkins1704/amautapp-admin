d09c122cdf892eacb7fca9dbf283fd7e35809537
renzo1704 <renzo@valere.io>
2023-04-06 11:51:40 -0500

    Correccion server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const Gun=require('gun');
-require('gun-mongo-key');
 const app=express();
 const path=require('path');
 const port=5000;
@@ -18,18 +17,4 @@ const server=app.listen(port,()=>{
 Gun({
     file:false,
     web:server,
-    mongo:{
-        host:'localhost',
-        port:'27017',
-        database:'repo-educacion-db',
-        collection: 'gun-mongo',
-        query:'',
-        opt: {
-            poolSize: 25 // how large is the connection pool
-            // include any other options when initializing:
-            // See: http://mongodb.github.io/node-mongodb-native/2.2/reference/connecting/connection-settings/
-        },
-        chunkSize: 250 // see below
-    },
-    // mongo:'mongodb://localhost:27017/repo-educacion-db',
 });
