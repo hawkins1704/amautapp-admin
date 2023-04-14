de45909e73f48456af3ef05d720f3eef5daa2cc3
renzo1704 <renzo@valere.io>
2023-04-13 21:41:13 -0500

    Creacion de ruta generica para todas las solicitudes de paginas diferentes a principal

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const port=process.env.PORT||5000;
 app.use(express.static(path.join(__dirname,"..","build")));
 app.use(express.static("public"));
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
 
 app.use(Gun.serve);
 
