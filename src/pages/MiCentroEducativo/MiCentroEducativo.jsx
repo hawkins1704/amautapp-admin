63da9ddb2c04b8529815947cbd67e63cf8c32900
Daniel <daniel_inmaculada@hotmail.com>
2023-05-05 11:01:47 -0500

    feature: Add MiCentroEducativo Component tables

--- src/pages/MiCentroEducativo/MiCentroEducativo.jsx
+++ src/pages/MiCentroEducativo/MiCentroEducativo.jsx
@@ -1,18 +1,58 @@
-import React from "react";
-import styles from "./styles.module.css";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../../components";
-import { Container, Typography } from "@mui/material";
+import { Container, Link, Typography } from "@mui/material";
+import { Link as RouterLink, useParams } from "react-router-dom";
+import { getAllAlumnosFake, getAllDocentesFake } from "../../services/fakeData";
+import styles from "./styles.module.css";
+import { TablaAlumnos, TablaDocentes } from "../CentroEducativo/components";
 
 const MiCentroEducativo = () => {
+    const params = useParams();
+    const centroEducativoId = params.centroEducativoId;
+    console.log(centroEducativoId);
+    const title = centroEducativoId;
+    const [alumnos, setAlumnos] = useState([]);
+    const [docentes, setDocentes] = useState([]);
     const breadcrumbs = [
         <Typography key="2" color="text.primary">
-            Mi Centro Educativo
+           {title}
         </Typography>,
     ];
+
+    
+    useEffect(() => {
+        const getInitialData = async () => {
+            if (process.env.REACT_APP_ENVIRONMENT === "development") {
+                //getAllDocentesSync(centroEducativoId, setDocentes);
+                //getAllAlumnosSync(centroEducativoId, setAlumnos);
+            } else if (process.env.REACT_APP_ENVIRONMENT === "production") {
+                //Temporalmente invertido para poder desplegar en netlify y ver fakeData
+                const alumnos = await getAllAlumnosFake(centroEducativoId);
+                const docentes = await getAllDocentesFake(centroEducativoId);
+                setAlumnos(alumnos);
+                setDocentes(docentes);
+            }
+        };
+        getInitialData();
+    }, []);
+    console.log("ALUMNOS: ",alumnos);
     return (
         <Layout title={"Mi centro educativo"} breadcrumbs={breadcrumbs}>
             <Container maxWidth={"xl"}>
-                <div>MiCentroEducativo</div>
+            {alumnos.length <= 0 ? (
+                    <div>Cargando</div>
+                ) : (
+                    <div className={styles.tableContainer}>
+                        <TablaDocentes rows={docentes} />
+                    </div>
+                )}
+                {docentes.length <= 0 ? (
+                    <div>Cargando</div>
+                ) : (
+                    <div className={styles.tableContainer}>
+                        <TablaAlumnos rows={alumnos} />
+                    </div>
+                )}
             </Container>
         </Layout>
     );
